989e1651e0f9c803a9d4fa3867777348085e24c4
Pratham Agarwal <32198580+Pratham1812@users.noreply.github.com>
2024-09-01 13:34:39 +0530

    Revert "feat: starter code for poker"

--- apps/web/games/poker/config.ts
+++ apps/web/games/poker/config.ts
@@ -21,5 +21,5 @@ export const pokerConfig = createZkNoidGameConfig({
   rules: 'Poker rules',
   runtimeModules: {}, // TEMPORARY!!!
   page: Poker,
-  externalUrl: 'http://localhost:3000/games/poker/global',
+  externalUrl: 'https://proto.zknoid.io/games/poker/global'
 });
